4cac2ddf7097b18fd7286a633bafe07886dffe11
Ishfaque Ahmed <engishfaque@yahoo.com>
2023-11-19 02:37:03 +0100

    Update QVD_Data_Analysis.py -> add absolute and relative path, and Exception handling.

--- QVD_Data_Analysis.py
+++ QVD_Data_Analysis.py
@@ -1,9 +1,9 @@
 """
 Program:        QVD Data Analysis Program
-Version:        1.0
+Version:        1.1
 Author:         Ishfaque Ahmed
-Date:           17.11.2023, Friday
-Description:    This Python script reads data from a QlikView QVD file using the qvd_reader module and prints the resulting DataFrame.
+Date:           19.11.2023, Sunday
+Description:    This Python script reads data from a QlikView QVD file using the qvd_reader module and prints the resulting DataFrame using pandas package
 
 Use cases:      - Data Exploration and Inspection
                 - Data Validation
@@ -11,21 +11,38 @@ Use cases:      - Data Exploration and Inspection
 
 """
 
-# Importing the qvd_reader module from the qvd package
-# qvd_reader module, which is being used to read data from a QVD file, returns the data in the form of a Pandas DataFrame therefore, pandas package needed
+# Importing the 'os' module, which provides a way to interact with the operating system.
+# This module allows tasks such as file and directory manipulation, working with environment variables, and more.
+import os
+
+# Importing the qvd_reader module from the qvd package.
+# qvd_reader module is used to read data from a QVD file, and it returns the data in the form of a DataFrame therefore, pandas package required.
 from qvd import qvd_reader
+
+# Importing the 'pandas' library with the alias 'pd'.
+# Pandas is a powerful data manipulation library and is used here for further analysis and manipulation of the data.
 import pandas as pd
 
-# Define the path and filename variables
-# Note: Replace the file path and file name with the actual path to your QVD file
-QVD_PATH = r'C:\Users\Documents\QVD'
-QVD_FILENAME = 'Test.qvd'
+# Define the relative or absolute path to the QVD file
+# Example of relative path: 'Data Sources\QVDs'
+# Example of absolute path: r'C:\Users\Documents\QVDs'
+# Example of server share folder path: r'\\Server1\DataSource'
+QVD_PATH = r'C:\Users\Documents\QVDs'
+QVD_FILENAME = 'F_Products.qvd'
 
-# Construct the full file path
-QVD_FILE_PATH = f'{QVD_PATH}\\{QVD_FILENAME}'
+# Check if the path is absolute
+if os.path.isabs(QVD_PATH):
+    # If absolute, use it directly
+    QVD_FILE_PATH = os.path.join(QVD_PATH, QVD_FILENAME)
+else:
+    # If relative, construct the absolute path
+    CURRENT_DIRECTORY = os.getcwd()
+    QVD_FILE_PATH = os.path.join(CURRENT_DIRECTORY, QVD_PATH, QVD_FILENAME)
 
 # Reading data from the QVD file and storing it in a DataFrame (df)
-df = qvd_reader.read(QVD_FILE_PATH)
-
-# Printing the DataFrame to inspect the loaded data
-print(df)
+try:
+    df = qvd_reader.read(QVD_FILE_PATH)
+    # Printing the DataFrame to inspect the loaded data
+    print(df)
+except Exception as e:
+    print(f"Error reading QVD file: {e}")
